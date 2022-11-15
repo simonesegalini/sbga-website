1efdc45118eebe4cded212304c291abccfba9368
simone <simonesegalini10@gmail.com>
2022-11-15 12:10:10 +0100

    wip

--- src/components/templates/RowsTemplate/RowsTemplate.tsx
+++ src/components/templates/RowsTemplate/RowsTemplate.tsx
@@ -8,9 +8,10 @@ import AboutTeamRowItem from "../../atoms/AboutTeamRowItem/AboutTeamRowItem";
 const RowsTemplate = (props: RowsPropsType) => {
   const { type, rows } = props;
   const styles = useRowsTemplateStyle();
+  console.log("ROWS -> ", rows);
 
   const Row = (row: any, index: number) => {
-    if (type === "aboutRow" || type === "teamRow" || type === "row") {
+    if (type === "aboutRow" || type === "teamRow") {
       return <AboutTeamRowItem index={index} {...row} />;
     }
 
