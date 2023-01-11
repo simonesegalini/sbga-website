07fd68a5588eaa9967fcd22e1822321d29b6f3c0
Simo <simonesegalini10@gmail.com>
2023-01-11 16:54:59 +0100

    wip

--- src/pages/homePage/hooks/useHomePageLogic.ts
+++ src/pages/homePage/hooks/useHomePageLogic.ts
@@ -1,17 +1,20 @@
-import { useHomePageStyle } from "../styles";
-import { useGlobal } from "../../../state/global/useGlobal";
-import { useNavigation } from "../../../navigation/useNavigation";
-import { useCallback } from "react";
-import { Paths } from "../../../navigation/types";
-import { useTranslation } from "react-i18next";
+import {useHomePageStyle} from "../styles";
+import {useGlobal} from "../../../state/global/useGlobal";
+import {useNavigation} from "../../../navigation/useNavigation";
+import {useCallback, useEffect, useRef} from "react";
+import {Paths} from "../../../navigation/types";
+import {useTranslation} from "react-i18next";
+import {usePageBottom} from "../../../hooks/usePageBottom";
 
 export const useHomePageLogic = () => {
   const { data: reducerData } = useGlobal();
   const { navigate } = useNavigation();
   const { t } = useTranslation();
   const { home } = reducerData!;
-
   const { img_thumbnail, categories } = home[0];
+  const styles = useHomePageStyle();
+  const reachedBottom = usePageBottom();
+  const isReached = useRef(false)
 
   const handleNavigation = useCallback(
     (path: Paths) => {
@@ -20,7 +23,16 @@ export const useHomePageLogic = () => {
     [navigate]
   );
 
-  const styles = useHomePageStyle();
+  useEffect( () => {
+    if(isReached.current) {
+      return;
+    }
+    if(reachedBottom && !isReached.current) {
+      isReached.current = true;
+      window.scrollTo(0, 0)
+      handleNavigation(Paths.Portfolio)
+    }
+  }, [handleNavigation, reachedBottom])
 
   return {
     img_thumbnail,
